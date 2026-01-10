3f1af52c7ce09badfc87d8afb3c7d249e4045c5f
Hafizuddin Nazlee <hafizuddin.nazlee@gmail.com>
2026-01-10 19:24:22 +0800

    clean up the repo so that we can start from scratch with the correct classes

--- src/main/java/com/lakarra/billing/repository/InvoiceRepository.java
+++
@@ -1,6 +0,0 @@
-package com.lakarra.billing.repository;
-
-import com.lakarra.billing.entity.Invoice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvoiceRepository extends JpaRepository<Invoice, Long> {}
--- src/main/java/com/lakarra/billing/repository/PaymentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.lakarra.billing.repository;
-
-import com.lakarra.billing.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {}
